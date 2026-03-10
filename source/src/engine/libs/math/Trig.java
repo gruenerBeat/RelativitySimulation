83c933d7cabd7b0f2d11a9901f711630776c7d43
Beat Grüner <beat.gruener@web.de>
2026-03-10 14:32:17 +0100

    Recalculated Jacobi Matrix

--- source/src/engine/libs/math/Trig.java
+++ source/src/engine/libs/math/Trig.java
@@ -3,7 +3,15 @@ package engine.libs.math;
 public class Trig {
     
     public static double cot(double x) {
-        return x == 0 ? 1000000 : 1 / Math.tan(x);
+        return Math.tan(x) == 0 ? 1000000 : 1 / Math.tan(x);
+    }
+
+    public static double sec(double x) {
+        return Math.cos(x) == 0 ? 1000000 : 1 / Math.cos(x);
+    }
+
+    public static double csc(double x) {
+        return Math.sin(x) == 0 ? 1000000 : 1 / Math.sin(x);
     }
 
     public static double degreeToRad(double a) {
--- source/src/game/Metric.java
+++ source/src/game/Metric.java
@@ -24,39 +24,61 @@ public class Metric {
     }
 
     public static Matrix getYDoublePrime(Vector y) {
+        double r = y.val[0];
+        double dr = y.val[1];
+        //double t = y.val[2];
+        double dt = y.val[3];
+        double h = y.val[4];
+        double dh = y.val[5];
+        //double p = y.val[6];
+        double dp = y.val[7];
+
         return new Matrix(new double[][]{
             {0, 1, 0, 0, 0, 0, 0, 0},
             {
-                M * y.val[3] * y.val[3] * (2 * y.val[0] - 6 * M) / Math.pow(y.val[0], 4)
-                + 2 * (2 * (y.val[0] - M)) * y.val[1] * y.val[1] / (y.val[0] * y.val[0] * (y.val[0] - 2 * M))
-                + y.val[5] * y.val[5] + Math.pow(Math.sin(y.val[4]), 2) * y.val[7] * y.val[7],
-                2 * M * y.val[1] / (y.val[0] * (y.val[0] - 2 * M)), 0,
-                -2 * M * (y.val[0] - 2 * M) / Math.pow(y.val[0], 3),
-                2 * (y.val[0] - 2 * M) * Math.sin(y.val[4]) * y.val[7] * y.val[7] * Math.cos(y.val[4]),
-                2 * (y.val[0] - 2 * M) * y.val[5], 0,
-                2 * (y.val[0] - 2 * M) * Math.pow(Math.sin(y.val[4]), 2) * y.val[7]
+                dp * dp * Math.pow(Math.sin(h), 2) + dh * dh - (rs * (2 * r - 3 * rs)) / (2 * Math.pow(r, 4)) * y.val[3] * y.val[3]
+                    + (rs * (rs - 2 * r)) / (2 * r * r * Math.pow(r - rs, 2)) * dr * dr,
+                rs / (r * r - r * rs) * dr,
+                0,
+                (rs * (r - rs)) / (Math.pow(r, 3)) * dt,
+                2 * dp * dp * Math.sin(h) * Math.cos(h) * (r - rs),
+                2 * dh * (r - rs),
+                0,
+                2 * dp * Math.pow(Math.sin(h), 2) * (r -rs)
             },
             {0, 0, 0, 1, 0, 0, 0, 0},
             {
-                4 * (M - y.val[0]) * y.val[1] * y.val[3] / (y.val[0] * y.val[0] * (y.val[0] - 2 * M)),
-                -2 * M * y.val[3] / (y.val[0] * (y.val[0] - 2 * M)), 0,
-                -2 * M * y.val[1] / (y.val[0] * (y.val[0] - 2 * M)), 0, 0, 0, 0
+                (rs * (2 * r - rs)) / (r * r * Math.pow(r - rs, 2)) * dr * dt,
+                -rs / (r * (r - rs)) * dt,
+                0,
+                -rs / (r * (r - rs)) * dr,
+                0,
+                0,
+                0,
+                0
+
             },
             {0, 0, 0, 0, 0, 1, 0, 0},
             {
-                2 * y.val[1] * y.val[5] / (y.val[0] * y.val[0]),
-                -2 * y.val[5] / y.val[0], 0, 0,
-                (Math.pow(Math.cos(y.val[4]), 2) - Math.pow(Math.sin(y.val[4]), 2)) * y.val[7] * y.val[7],
-                -2 * y.val[1] / y.val[0], 0,
-                2 * Math.sin(y.val[4]) * Math.cos(y.val[4]) * y.val[5]
+                2 / (r * r) * dr * dh,
+                -2 / r * dh,
+                0,
+                0,
+                dp * dp * (Math.pow(Math.cos(h), 2) - Math.pow(Math.sin(h), 2)),
+                -2 / r * dr,
+                0,
+                2 * dp * Math.sin(h) * Math.cos(h) 
             },
             {0, 0, 0, 0, 0, 0, 0, 1},
             {
-                -2 * y.val[1] * y.val[7] / (y.val[0] * y.val[0]),
-                -2 * y.val[7] / y.val[0], 0, 0,
-                2 * (1 + Math.pow(Trig.cot(y.val[4]), 2)) * y.val[5] * y.val[7],
-                -2 * Trig.cot(y.val[4]) * y.val[7], 0,
-                -2 * Trig.cot(y.val[4]) * y.val[5],
+                2 / (r * r) * dr * dp,
+                -2 / r * dp,
+                0,
+                0,
+                2 * dh * dp * Math.pow(Trig.csc(h), 2),
+                -2 * dp * Trig.cot(h),
+                0,
+                -(2 * (dh * r * Trig.cot(h) + dr)) / r
             }
         }, 8, 8);
     }
